ea849aa56535b448206368354d2a95e9bf95515f
Vedant Chainani <vedantchainani1084@gmail.com>
2024-05-03 22:38:56 +0530

    chore: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className=''>
+      <div className='h-screen bg-red-200'></div>
+      <div className='h-screen bg-emerald-200'></div>
+      <div className='h-screen bg-cyan-200'></div>
+    </div>
+  );
+};
+
+export default Home;

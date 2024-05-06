f2a8e69f1fcefc428385555682d3fb539764451b
Vedant Chainani <vedantchainani1084@gmail.com>
2024-05-06 20:16:58 +0530

    chore: update animations

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -4,8 +4,18 @@ import { type Variants, motion } from 'framer-motion';
 import { Navbar } from '~/components';
 
 const variants: Variants = {
-  hidden: { opacity: 0, y: 30, scale: 0.98 },
-  enter: { opacity: 1, y: 0, scale: 1 },
+  hidden: {
+    opacity: 0,
+    y: 30,
+    scale: 0.98,
+    clipPath: 'circle(10% at 0 0)',
+  },
+  enter: {
+    opacity: 1,
+    y: 0,
+    scale: 1,
+    clipPath: 'circle(145% at 100% 100%)',
+  },
 };
 
 export default function Template({ children }: { children: React.ReactNode }) {
